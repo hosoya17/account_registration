d0caa44e231667d520d484fa684bd9e49aead6a8
hosoya <100053674+hosoya17@users.noreply.github.com>
2023-07-19 20:55:14 +0900

    Create index.js

---
+++ JavaScript/index.js
@@ -0,0 +1,51 @@
+"use strict";
+const firstName = document.querySelector('#firstName');
+const givenName = document.querySelector('#givenName');
+const firstNameKana = document.querySelector('#firstNameKana');
+const givenNameKana = document.querySelector('#givenNameKana');
+const age = document.querySelector('#age');
+const gender = document.querySelector('input[type=radio]');
+const telNum1 = document.querySelector('.telNum1');
+const telNum2 = document.querySelector('.telNum2');
+const telNum3 = document.querySelector('.telNum3');
+
+const zip1 = document.querySelector('#zip1');
+const zip2 = document.querySelector('#zip2');
+const prefectures = document.querySelector('select[name="prefectures"]');
+const city = document.querySelector('#city');
+const address = document.querySelector('#address');
+const button = document.querySelector('input[type="button"]');
+const msg = document.querySelector('.msg');
+const kaku = document.querySelector('.kakunin');
+
+let zipurl;
+let url = 'https://zipcloud.ibsnet.co.jp/api/search?zipcode=';
+
+button.addEventListener('click', () => {
+  msg.style.color = 'black';
+  msg.innerHTML = '検索中...';
+  zipurl = url + zip1.value + zip2.value;
+
+  fetch(zipurl)
+    .then(res => {
+      return res.json();
+    })
+    .then(data => {
+      if (data.status === 400) {
+        if (data.message.includes("桁数が不正")) {
+          msg.style.color = 'red';
+          msg.innerHTML = '「郵便番号」の桁数が不正です';
+        } else if (data.message.includes("数字以外の文字が指定されています")) {
+          msg.style.color = 'red';
+          msg.innerHTML = '「郵便番号」に数字以外の文字が指定されています。';
+        }
+      } else if (data.results === null) {
+        msg.style.color = 'red';
+        msg.innerHTML = '存在しない郵便番号です。';
+      } else {
+        msg.innerHTML = '';
+        prefectures.value = data.results[0].address1;
+        city.value = data.results[0].address2 + data.results[0].address3;
+      }
+    });
+});
